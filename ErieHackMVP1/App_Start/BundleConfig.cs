3e0754653da576cda3e63bcaf79627819bf6a1d9
cataclysm1987 <conklin.eric@gmail.com>
2017-03-30 10:21:30 -0400

    add bootstrap theme and update homepage

--- ErieHackMVP1/App_Start/BundleConfig.cs
+++ ErieHackMVP1/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ErieHackMVP1
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cerulean.css",
                       "~/Content/site.css"));
         }
     }
